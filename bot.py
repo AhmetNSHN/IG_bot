3abc7205c9931b986d14a8c6aac394f0e941dcdb
Ahmet Naci Sahin <ahmetnsahin203@gmail.com>
2023-05-31 14:49:42 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,99 +1,76 @@
-import os
 import time
 from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
 
+TIME_OUT = 15
 
 class unfolllowlist_maker:
     def __init__(self):
-        self.USERNAME = '' #-> your username here
-        self.PASSWORD = '' #-> your password here
-        usr = "" #-> username that will be checked here
-        self.bot = webdriver.Safari()
+        self.USERNAME = ''  # -> your username here
+        self.PASSWORD = ''  # -> your password here
+        self.scraped_user = ''  # -> username that will be checked here
+
+        # selenium configurations ------------------------------------------------
+        caps = DesiredCapabilities().CHROME
+        caps["pageLoadStrategy"] = "eager"
+        self.bot = webdriver.Chrome(desired_capabilities=caps)
+        options = webdriver.ChromeOptions()
+        mobile_emulation = {
+            "userAgent": 'Mozilla/5.0 (Linux; Android 4.0.3; HTC One X Build/IML74K) AppleWebKit/535.19'
+                         ' (KHTML, like Gecko) Chrome/83.0.1025.133 Mobile Safari/535.19'
+        }
+        options.add_experimental_option("mobileEmulation", mobile_emulation)
+        self.bot.get("https://instagram.com/")
+        self.bot.set_window_size(1500, 1000)
 
         self.login()
 
         self.followers = []
-        turn = '//*[@id="react-root"]/section/main/div/header/section/ul/li[2]/a/span'
-        self.scrape(usr, turn, self.followers)
+        turn = "//a[contains(@href, '/followers')]"
+        self.scrape(turn, self.followers)
 
-        # self.bot.find_element_by_xpath(
-        #     '/html/body/div[4]/div/div/div[1]/div/div[2]/button').click()
+        self.bot.find_element(By.CSS_SELECTOR, '.x1qjc9v5.x9f619.x78zum5.xdt5ytf.x1iyjqo2.xl56j7k').click()
+        time.sleep(2)
 
         self.followed = []
-        turn = '//*[@id="react-root"]/section/main/div/header/section/ul/li[3]/a/span'
-        self.scrape(usr, turn, self.followed)
+        turn = "//a[contains(@href, '/following')]"
+        self.scrape(turn, self.followed)
+
         self.unfollow_list = []
         self.not_following()
 
-
     def login(self):
-        options = webdriver.ChromeOptions()
-        # options.add_argument("--headless")
 
-        mobile_emulation = {
-            "userAgent": 'Mozilla/5.0 (Linux; Android 4.0.3; HTC One X Build/IML74K) AppleWebKit/535.19 (KHTML, like Gecko) Chrome/83.0.1025.133 Mobile Safari/535.19'
-        }
-        options.add_experimental_option("mobileEmulation", mobile_emulation)
-
-        self.bot.get("https://instagram.com/")
-        self.bot.set_window_size(1500, 1000)
-        time.sleep(5)
-        self.bot.find_element_by_xpath(
-            '//*[@id="loginForm"]/div/div[3]').click()
         print("Logging in...")
-        time.sleep(1)
-        username_field = self.bot.find_element_by_xpath(
-            '//*[@id="loginForm"]/div/div[1]/div/label/input')
+        # USERNAME
+        username_field = WebDriverWait(self.bot, 15).until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div/div[1]/div/label/input')))
         username_field.send_keys(self.USERNAME)
+        # PASSWORD
+        password_field = WebDriverWait(self.bot, TIME_OUT).until(EC.presence_of_element_located((By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')))
+        password_field.send_keys(self.PASSWORD)
+        # LOGIN
+        WebDriverWait(self.bot, TIME_OUT).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="loginForm"]/div/div[3]/button/div'))).click()
 
-        find_pass_field = (
-            By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
-        WebDriverWait(self.bot, 100).until(
-            EC.presence_of_element_located(find_pass_field))
-        pass_field = self.bot.find_element(*find_pass_field)
-        WebDriverWait(self.bot, 1).until(
-            EC.element_to_be_clickable(find_pass_field))
-        pass_field.send_keys("0")
-        # bot.find_element_by_xpath(
-        #     '///*[@id="loginForm"]/div/div[3]n').click()
-        time.sleep(2)
+    def scrape(self, turn, list): #scrapes usernames from followers and following popups
 
-        find_pass_field = (
-            By.XPATH, '//*[@id="loginForm"]/div/div[2]/div/label/input')
-        WebDriverWait(self.bot, 100).until(
-            EC.presence_of_element_located(find_pass_field))
-        pass_field = self.bot.find_element(*find_pass_field)
-        WebDriverWait(self.bot, 1).until(
-            EC.element_to_be_clickable(find_pass_field))
-        pass_field.send_keys(self.PASSWORD)
-        self.bot.find_element_by_xpath(
-            '//*[@id="loginForm"]/div/div[3]/button/div').click()
         time.sleep(5)
-
-    def scrape(self, username, turn, list):
-
-        link = 'https://www.instagram.com/{}/'.format(username)
+        link = 'https://www.instagram.com/{}/'.format(self.scraped_user)
         self.bot.get(link)
-        time.sleep(5)
-        print('button will be clicked...')
-        self.bot.find_element_by_xpath(turn).click()# button
+        WebDriverWait(self.bot, TIME_OUT).until(EC.presence_of_element_located((By.XPATH, turn))).click()
         print('button clicked')
-        time.sleep(5)
+        time.sleep(3)
         print('Scrapping...')
 
         ActionChains(self.bot).send_keys(Keys.END).perform()
         time.sleep(3)
 
-        flv = self.bot.find_elements_by_xpath(
-            '//*[@id="react-root"]/section/main/div/ul/div/li/div/div[1]/div[2]/div[1]/a')
         self.scroller()
-        followers = self.bot.find_elements_by_css_selector(".FPmhX.notranslate._0imsa")
+        followers = self.bot.find_elements(By.CSS_SELECTOR, ".x9f619.xjbqb8w.x1rg5ohu.x168nmei.x13lgxp2.x5pf9jr.xo71vjh.x1n2onr6.x1plvlek.xryxfnj.x1c4vz4f.x2lah0s.x1q0g3np.xqjyukv.x6s0dn4.x1oa3qoh.x1nhvcw1")
 
         i = 0
         for f in followers:
@@ -102,9 +79,10 @@ class unfolllowlist_maker:
             i = i + 1
         print(f"scraped users in first part: {i} . Be sure that number of user match with instagram statistics otherwise check your internet connection")
 
-    def scroller(self):
+
+    def scroller(self): # scrolls followers and following popups
         command = """
-            page = document.querySelector(".isgrP");
+            page = document.querySelector("._aano");
             page.scrollTo(0, page.scrollHeight);
             var page_end = page.scrollHeight;
             return page_end;
@@ -112,12 +90,11 @@ class unfolllowlist_maker:
         page_end = self.bot.execute_script(command)
         while True:
             end = page_end
-            time.sleep(1)
+            time.sleep(2)
             page_end = self.bot.execute_script(command)
-            print("scraping>>>")
+            # print("scraping>>>")
             if end == page_end:
                 break
-
 
     def not_following(self):
         for f in self.followed:
@@ -128,4 +105,3 @@ class unfolllowlist_maker:
 
 run = unfolllowlist_maker()
 # @ahmetncsahin
-
